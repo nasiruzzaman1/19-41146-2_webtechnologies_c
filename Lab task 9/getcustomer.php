be326229097588bae5960b363587eb8986ad54a8
nasiruzzaman1 <91921542+nasiruzzaman1@users.noreply.github.com>
2022-08-23 23:31:43 +0600

    Add files via upload

---
+++ Lab task 9/getcustomer.php
@@ -0,0 +1,48 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "product_db");
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+
+$sql = "SELECT id, departure, destination, journeydate, returndate,class
+FROM ticket WHERE id = ?";
+
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("i", $_GET['q']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($id, $departure, $destination, $journeydate, $returndate, $class, );
+$stmt->fetch();
+$stmt->close();
+
+echo "<table>
+  <tr>
+    <th>Ticket ID</th>
+    <th>Departure</th> 
+    <th>Destination</th>
+  </tr>
+  <tr>
+    <td>" . $id . "</td>
+    <td>" . $departure . "</td>
+    <td>" . $destination . "</td>
+  </tr>
+  <tr>
+    
+  </tr>
+  <tr>
+    
+  </tr>
+</table>";
+?>
+
+
+
+
+
+
+
+
+
+
+
+
